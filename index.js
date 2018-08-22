8cd206c7dd94f21ba5f6028d9fb1148d3e5a325f
Sarah Schaller <sarahcschaller@gmail.com>
2018-08-22 13:53:01 +0000

    Done.

---
+++ index.js
@@ -0,0 +1,26 @@
+function getFirstSelector(selector) {
+  return document.querySelector(selector)
+};
+
+function nestedTarget(nested) {
+  return document.querySelector('#nested .target')
+};
+
+function deepestChild() {
+  let grandNode = document.getElementById('grand-node');
+  let nextNode = grandNode.children[0];
+  while (nextNode) {
+    grandNode = nextNode
+    nextNode = grandNode.children[0]
+  }
+  return grandNode
+}
+
+function increaseRankBy(n) {
+  const rankedList = document.querySelectorAll('.ranked-list')
+
+  for (let i = 0; i < rankedList.length; i++) {
+    let rankedListValue = parseInt(rankedList[i].innerHTML);
+    rankedList[i].innerHTML = rankedListValue + n;
+  }
+}
